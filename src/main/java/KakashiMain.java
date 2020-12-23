7e044164692b777b21c00fbd35359b78e49e7018
DragonWang <185138868@qq.com>
2020-12-23 11:01:12 +0800

    提交maven项目配置

---
+++ src/main/java/KakashiMain.java
@@ -0,0 +1,13 @@
+/**
+ * <功能描述>
+ *
+ * @author jialong.wang
+ * @Date on 2020/12/23 11:00 AM
+ * @since 1.0
+ */
+public class KakashiMain {
+
+    public static void main(String[] args) {
+        System.out.println("测试主类");
+    }
+}
